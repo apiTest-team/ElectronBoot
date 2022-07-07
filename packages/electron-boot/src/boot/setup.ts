3b49f3d3c57f1d41b8bab226b358659bdcf2e7e8
liuqin <876771120@qq.com>
2022-07-08 03:07:36 +0800

    1.electron启动装饰器

--- packages/electron-boot/src/boot/setup.ts
+++ packages/electron-boot/src/boot/setup.ts
@@ -1,6 +1,21 @@
+import util from "util";
+import {AutowiredContainer,bindContainer} from "@autowired/core";
+import {BootstrapOptions} from "./options";
+
+//debug工具
+const debug = util.debuglog('electron:debug');
 /**
- * 初始化全局context
+ * 初始化应用上下文
+ * @param globalOptions
  */
-export const initializeGlobalApplicationContext = async ()=>{
+export const prepareGlobalApplicationContext =  (globalOptions:BootstrapOptions) => {
+    debug('[electron-boot]: start "initializeGlobalApplicationContext"');
+    debug(`[electron-boot]: bootstrap options = ${util.inspect(globalOptions)}`);
+    // 创建上下文
+    const applicationContext = globalOptions.applicationContext  ?? new AutowiredContainer()
+    debug('[electron-boot]: delegate module map from decoratorManager');
+    // 绑定容器
+    bindContainer(applicationContext)
+    global["ELECTRON_APPLICATION_CONTEXT"] = applicationContext
 
 }
